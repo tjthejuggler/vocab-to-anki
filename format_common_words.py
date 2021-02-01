66be938c028048bd9c0c8d795a10b8710a2e6e1e
Timothy Ross <tjthejuggler@gmail.com>
2021-01-31 19:47:11 -0600

    dl mp3

---
+++ format_common_words.py
@@ -0,0 +1,19 @@
+from os import path
+
+lang = 'tr'
+file = open( "commonwords.txt", "r")
+lines = file.readlines()
+file.close()
+
+formated_lines = []
+
+for linenumber in range (0,len(lines)):
+	split_line = lines[linenumber].split()
+	print(split_line)
+	formated_line = split_line[1] + ' - ' + split_line[2] + ' - no hint\n'
+	formated_lines.append(formated_line)
+
+
+with open('commonwords_formated.txt', 'w') as f:
+    for item in formated_lines:
+        f.write("%s" % item)
