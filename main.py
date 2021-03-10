ce554ef54c179120338987a20f245cde9db906ee
Timothy Ross <tjthejuggler@gmail.com>
2021-03-10 12:30:31 -0600

    audio lesson entries repetition numbers based on audio clip length

--- main.py
+++ main.py
@@ -530,16 +530,21 @@ def check_for_and_try_to_get_mp3s(first_word, first_lang, second_word, second_la
 
 def create_normal_audio_lesson_entry(first_sound, second_sound, long_silence, short_silence):
 	audio = AudioSegment.silent(duration=10)
-	audio += first_sound + long_silence + second_sound + short_silence
-	audio += second_sound + long_silence + second_sound + short_silence + second_sound
+	if second_sound.duration_seconds < 3:
+		audio += first_sound + long_silence + second_sound + short_silence + second_sound
+	else:
+		audio += first_sound + long_silence + second_sound + short_silence
+		audio += second_sound + long_silence + second_sound + short_silence + second_sound
 	return audio
-#finish making create_reverse..
-#figure out how we want to hook up each of these audio lesson entries to length of sounds
-#	short sounds get repeated fewer times
+
 def create_reverse_audio_lesson_entry(first_sound, second_sound, long_silence, short_silence):
 	audio = AudioSegment.silent(duration=10)
-	audio += second_sound + long_silence + second_sound + short_silence
-	audio += second_sound + long_silence + second_sound + short_silence + second_sound
+	if first_sound.duration_seconds < 3:
+		audio += second_sound + long_silence + second_sound + short_silence + first_sound
+		audio += short_silence + second_sound
+	else:
+		audio += second_sound + long_silence + second_sound + short_silence + first_sound
+		audio += long_silence + second_sound + short_silence + second_sound
 	return audio
 
 def prepare_audio_lesson_item(first_word, first_lang, second_word, second_lang, hint, audio_text, audio_lesson_order_dict):
@@ -551,9 +556,9 @@ def prepare_audio_lesson_item(first_word, first_lang, second_word, second_lang, 
 	else:
 		random_bit = random.getrandbits(1)
 		use_normal_order = bool(random_bit)
-	print('making audio', prim_word, second_word)
-	first_sound = AudioSegment.from_mp3(pron_fold+'/'+second_word+'/'+second_lang+'.mp3')
-	second_sound = AudioSegment.from_mp3(pron_fold+'/'+first_word+'/'+first_lang+'.mp3')
+	print('making audio', first_word, second_word)
+	first_sound = AudioSegment.from_mp3(pron_fold+'/'+second_lang+'/'+second_word+'.mp3')
+	second_sound = AudioSegment.from_mp3(pron_fold+'/'+first_lang+'/'+first_word+'.mp3')
 	long_silence = AudioSegment.silent(duration=second_sound.duration_seconds*2000)					
 	short_silence = AudioSegment.silent(duration=second_sound.duration_seconds*1000)
 	if use_normal_order:
